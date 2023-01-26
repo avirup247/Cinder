00bbc1276b521d39d8b3c8fbaeab546ea5a2177c
avirup chakraborty <avirupchakraborty247@gmail.com>
2023-01-26 07:57:48 -0800

    this the commit for muni aj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Greet from './components/greet';
 import Welcome from './components/welcome';
-
+import Message from './components/messege';
 function App() {
   return (
     <div className="App">
      <Greet name="bruce" title="chakraborty"></Greet> 
      <Welcome/>
+     <Message name="muni"/>
     </div>
   );
 }
